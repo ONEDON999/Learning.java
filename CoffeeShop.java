2162edde4e88392a5715241dd7d53be488d982c5
ONEDON <niigyanchristian@gmail.com>
2021-04-16 09:30:26 +0000

    ONEDON CoffeeShop

--- CoffeeShop.java
+++ CoffeeShop.java
@@ -1,7 +1,5 @@
 package com.foo;
 
-import com.work.MenuItem;
-
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -10,14 +8,18 @@ public class CoffeeShop {
         Scanner scanner=new Scanner(System.in);
         Scanner scanner1=new Scanner(System.in);
 
-
+//allows user to input the name of one of te item needed
         System.out.println("Please enter the name of item you want");
         String itemName=scanner1.nextLine();
+
+        //Items in the my shop
         String d="egg";
         String e="bread";
         String f="tea";
         String g="storm";
         String h="malt";
+
+        //Allow the user to make some order if what the he or she needs is available
         if (itemName.toLowerCase(Locale.ROOT).equals(d)||itemName.equals(e)||
                 itemName.equals(f)||itemName.equals(g)||itemName.equals(h)) {
             String Name = "Egg";
@@ -40,6 +42,7 @@ public class CoffeeShop {
             String Type4 = "Drink";
             String Price4 = "GHS100";
 
+//Table form of the items and it prices
             System.out.println("The list of items in ONEDON's shop");
             System.out.println("Name        Type        Price");
             System.out.println(Name + "         " + Type + "        " + Price);
@@ -48,31 +51,46 @@ public class CoffeeShop {
             System.out.println(Name3 + "       " + Type3 + "       " + Price3);
             System.out.println(Name4 + "   " + Type4 + "       " + Price4);
 
-            System.out.println("Please enter the number of items you want each orderly: ");
+//allow the user to enter the number of items in each of the items int the orderly form(from top downwards)
+            System.out.println("Please enter the number of items you want each from top downwards orderly: ");
             int egg = scanner.nextInt();
             int bread = scanner.nextInt();
             int tea = scanner.nextInt();
             int storm = scanner.nextInt();
             int malt = scanner.nextInt();
+
+            //Calculation for the total price
           int  total=egg*200+bread*300+300*tea+70*storm+100*malt;
-            System.out.println("list of food items you want to order");
+
+          // displays the type of item the user wants to order
+            System.out.println("list of food items you want to order;");
 
 if (egg>0){ System.out.println("Egg(s)");}
 if(bread>0){ System.out.println("Bread"); }
 if(tea>0){ System.out.println("Cup of tea"); }
-            System.out.println("list of Dink(s) you want to order");
+            System.out.println("list of Dink(s) you want to order;");
 if(storm>0){ System.out.println("Storm energy drink");}
 if(malt>0){ System.out.println("Malt");}
+
+//display the total cost of the items ordered
 System.out.println("Please the total Amount your item cost is GHS"+total+".000");
-            System.out.println("Kindly enter your amount");
+
+//receiving the amount from the user.
+            System.out.println("Kindly enter your amount: ");
 int amount=scanner.nextInt();
 if(amount==total){
     System.out.println("Please the items are ready, THANK YOU");
-}else {
+}else if (amount>total){
+    System.out.println("Please your items are ready,your balance is GHS"+(amount-total)+", THANK YOU.");
+}
+
+//if the user amount cant buy the items selected
+else {
     System.out.println("Your amount could not reach the required amount,sorry your order failed");
 }
 
         }
+        //if the user item is not available
         else {
             System.out.println("Please this item is currently unavailable");
         }
